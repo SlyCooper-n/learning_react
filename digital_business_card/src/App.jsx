a81fee7d734d92be566da3c4f33b47f30ec83960
Gabriel <gabrielvitor.frasao@gmail.com>
2022-04-21 11:56:23 -0300

    50% completed

--- digital_business_card/src/App.jsx
+++ digital_business_card/src/App.jsx
@@ -1,5 +1,24 @@
+import Info from "./components/Info";
+import MainText from "./components/MainText";
+import Footer from "./components/Footer";
+
 function App() {
-  return <h1>Hello, React!</h1>;
+  return (
+    <>
+      <div className="w-3/4 mx-auto flex flex-col items-center bg-gray-700 text-slate-100 rounded-md">
+        <Info />
+        <MainText />
+        <Footer />
+      </div>
+
+      <style jsx>{`
+        div.mx-auto {
+          max-width: 500px;
+          width: 75%;
+        }
+      `}</style>
+    </>
+  );
 }
 
 export default App;
