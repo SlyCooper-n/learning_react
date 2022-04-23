78a350d05f7ca556c4cfe7c0c899ed57f64b0483
Gabriel <gabrielvitor.frasao@gmail.com>
2022-04-23 16:51:37 -0300

    .

--- meme_generator/src/components/Meme.jsx
+++ meme_generator/src/components/Meme.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
+import memeData from "../memeData";
+
 export default function Meme() {
+    const [meme, setMeme] = useState({
+            topText: "",
+            bottomText: "",
+            randomImg: "",
+        }),
+        [allMemeData, setAllMemeData] = useState(memeData);
+
+    function getMeme() {
+        const num = Math.floor(Math.random() * memeData.data.memes.length),
+            { url } = memeData.data.memes[num];
+
+        setMeme((prevMeme) => ({
+            ...prevMeme,
+            randomImg: url,
+        }));
+    }
+
     return (
-        <main>
-            <form
-                action=""
-                className="w-1/2 py-16 mx-auto grid grid-cols-2 gap-4"
-            >
+        <main className="w-1/2 mx-auto">
+            <div className="w-full mx-auto py-16 grid grid-cols-2 gap-4">
                 <input
                     type="text"
                     placeholder="Top text"
@@ -16,10 +33,15 @@ export default function Meme() {
                     className="p-2 border border-neutral-300 rounded-sm"
                 />
 
-                <button className="py-2 col-start-1 col-end-3 bg-gradient-to-r from-amber-700 to-amber-500 text-slate-50 font-semibold rounded-sm">
+                <button
+                    onClick={getMeme}
+                    className="py-2 col-start-1 col-end-3 bg-gradient-to-r from-amber-700 to-amber-500 text-slate-50 font-semibold rounded-sm"
+                >
                     Get a new meme image 🖼
                 </button>
-            </form>
+            </div>
+
+            <img src={meme.randomImg} alt="alg" className="mx-auto" />
         </main>
     );
 }
