6a7b50f485a3201e0ba58d9835c800f320fb3595
Gabriel <gabrielvitor.frasao@gmail.com>
2022-04-25 21:29:38 -0300

    feature to hold die

--- tenzies/src/components/Die.jsx
+++ tenzies/src/components/Die.jsx
@@ -1,7 +1,12 @@
 export default function Die(props) {
     return (
-        <div className="w-20 aspect-square flex justify-center items-center bg-white text-3xl font-semibold rounded-2xl shadow-md">
-            {props.number}
+        <div
+            onClick={() => props.onClick(props.number.id)}
+            className={`w-20 aspect-square flex justify-center items-center ${
+                props.number.isHeld ? "bg-green-500" : "bg-white"
+            } text-3xl font-semibold rounded-2xl shadow-md`}
+        >
+            {props.number.value}
         </div>
     );
 }
