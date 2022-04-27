f308d6c883d929a290b43cfa026485601507ec14
Gabriel <gabrielvitor.frasao@gmail.com>
2022-04-27 18:49:08 -0300

    feature to hold dice fixed

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -3,34 +3,37 @@ import { useEffect, useId, useState } from "react";
 import Die from "./components/Die";
 
 function App() {
-    const [diceNumbers, setDiceNumbers] = useState(() => getRandomNumbers());
+    const [diceNumbers, setDiceNumbers] = useState(() => allNewDice());
 
-    function getRandomNumbers() {
+    function generateNewDie() {
+        return {
+            id: nanoid(),
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+        };
+    }
+
+    function allNewDice() {
         let newArr = [];
 
         for (let i = 0; i < 12; i++) {
-            let heldValue;
-            if (diceNumbers[i].isHeld) {
-                heldValue = diceNumbers[i].value;
-            }
-
-            newArr.push({
-                id: nanoid(),
-                value: heldValue ?? Math.ceil(Math.random() * 6),
-                isHeld: diceNumbers[i].isHeld || false,
-            });
+            newArr.push(generateNewDie());
         }
-
-        // diceNumbers.map(die => {
-        //     return die.isHeld == true ? {...die, value: }
-        // });
 
         return newArr;
     }
 
+    function rollDice() {
+        setDiceNumbers((prevDiceNumbers) =>
+            prevDiceNumbers.map((die) => {
+                return die.isHeld ? die : generateNewDie();
+            })
+        );
+    }
+
     function holdDie(id) {
-        setDiceNumbers(
-            diceNumbers.map((die) => {
+        setDiceNumbers((prevDiceNumbers) =>
+            prevDiceNumbers.map((die) => {
                 return die.id == id
                     ? {
                           ...die,
@@ -48,14 +51,21 @@ function App() {
 
     return (
         <main className="mx-auto p-8 text-center bg-slate-200 rounded-md">
-            <div className="flex justify-center items-center">
+            <h1 className="mb-4 text-5xl font-bold text-slate-800">Tenzies</h1>
+
+            <p className="w-3/4 mx-auto text-xl font-semibold text-slate-600">
+                Roll until all dice are the same. Click each die to freeze it at
+                its current value between rolls.
+            </p>
+
+            <div className="my-4 flex justify-center items-center">
                 <div className="p-8 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-7">
                     {diceElements}
                 </div>
             </div>
 
             <button
-                onClick={() => setDiceNumbers(getRandomNumbers())}
+                onClick={rollDice}
                 className="px-10 py-3 bg-blue-900 text-slate-100 text-xl font-semibold rounded-md"
             >
                 Roll
