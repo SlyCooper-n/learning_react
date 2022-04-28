90e94fd74145da8262d45d0ebb7e823b9f009bb5
Gabriel <gabrielvitor.frasao@gmail.com>
2022-04-28 18:48:13 -0300

    Home page completed

--- quizzical_trivia/src/App.jsx
+++ quizzical_trivia/src/App.jsx
@@ -1,5 +1,15 @@
+import { useState } from "react";
+import Home from "./components/Home";
+import Questions from "./components/Questions";
+
 function App() {
-    return <div className="App"></div>;
+    const [home, setHome] = useState(true);
+
+    function startQuiz() {
+        setHome(false);
+    }
+
+    return <>{home ? <Home onClick={startQuiz} /> : <Questions />}</>;
 }
 
 export default App;
