0b90e0b277533b11396bdce32e87a447989832d6
Gabriel <gabrielvitor.frasao@gmail.com>
2022-04-28 16:45:46 -0300

    feature to track the time

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -5,20 +5,34 @@ import Die from "./components/Die";
 function App() {
     const [diceNumbers, setDiceNumbers] = useState(() => allNewDice()),
         [tenzies, setTenzies] = useState(false),
-        [turn, setTurn] = useState(0);
+        [turn, setTurn] = useState(0),
+        [bestTime, setBestTime] = useState(() => {
+            return localStorage.getItem("bestTime") ?? "No records";
+        }),
+        [time, setTime] = useState(0);
 
     useEffect(() => {
+        // initializes the start time
         localStorage.setItem("startTime", new Date().getTime());
     }, []);
 
     useEffect(() => {
-        const endTime = new Date().getTime();
-        const startTime = localStorage.getItem("startTime");
-        const time = ((endTime - startTime) / 1000).toFixed(2);
-        console.log(time);
+        // makes the track of time and best time
+        const endTime = new Date().getTime(),
+            startTime = localStorage.getItem("startTime"),
+            time = ((endTime - startTime) / 1000).toFixed(2),
+            bestTime = localStorage.getItem("bestTime") ?? "No records";
+
+        setTime(time);
+
+        if (time != 0 && time < bestTime) {
+            localStorage.setItem("bestTime", time);
+            setBestTime(localStorage.getItem("bestTime"));
+        }
     }, [tenzies]);
 
     useEffect(() => {
+        // checks if the game is finished
         let num = diceNumbers[0].value;
         let bool = diceNumbers.every((die) => die.isHeld && die.value == num);
 
@@ -89,19 +103,26 @@ function App() {
                 its current value between rolls.
             </p>
 
+            <span className="mt-8 block font-semibold text-slate-800">
+                Best time:{" "}
+                {bestTime == "No records" ? bestTime : `${bestTime} seconds`}
+            </span>
+
             <div className="my-4 flex justify-center items-center">
                 <div className="p-8 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-7">
                     {diceElements}
                 </div>
             </div>
 
-            {turn != 0 && !tenzies ? (
-                <span className="block mb-8 font-semibold">
+            {turn > 0 && !tenzies && (
+                <span className="block mb-8 font-semibold text-slate-800">
                     {turn} {turn == 1 ? "Roll" : "Rolls"}
                 </span>
-            ) : (
-                <span className="block mb-8 font-semibold">
-                    You won in {turn} Rolls
+            )}
+
+            {tenzies && (
+                <span className="block mb-8 font-semibold text-slate-800">
+                    You won in {turn} Rolls and {time} seconds.
                 </span>
             )}
 
